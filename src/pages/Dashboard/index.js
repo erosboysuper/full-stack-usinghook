cbb725a6a00318d059f4118e93a05544ca739b36
erosboysuper <erosboysuper@gmail.com>
2021-08-29 06:21:06 -0700

    first commit

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react"
+import { Container } from "reactstrap"
+
+const Dashboard = props => {
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid>
+          <h4>Dashboard</h4>
+        </Container>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Dashboard
